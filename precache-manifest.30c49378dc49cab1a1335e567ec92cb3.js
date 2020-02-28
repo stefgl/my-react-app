b039843ea8c0e31cf3a564d528f288406b5adabd
Stefan Glaum <stefan@glaum.de>
2020-02-28 07:39:03 +0100

    Updates

---
+++ precache-manifest.30c49378dc49cab1a1335e567ec92cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f1949e4fa70e40d31c1cd549c9bdec",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "a62c3d4bb91eae89e310",
+    "url": "/my-react-app/static/css/main.e923315b.chunk.css"
+  },
+  {
+    "revision": "096f181a5d6aaa131f17",
+    "url": "/my-react-app/static/js/2.029268c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-react-app/static/js/2.029268c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62c3d4bb91eae89e310",
+    "url": "/my-react-app/static/js/main.aa66b07c.chunk.js"
+  },
+  {
+    "revision": "3916136fee8b9c9b5407",
+    "url": "/my-react-app/static/js/runtime-main.45ed5c2f.js"
+  }
+]);
